109be1b24ba96ae519770184c30b475d8adaa58a
Catheryne Small <catheryn.small@gmail.com>
2020-05-15 05:38:54 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1 
   end
-  
+ 
+  if block_return_values.include?(false)
+    flase
+  else
+    true 
+  end
 end
